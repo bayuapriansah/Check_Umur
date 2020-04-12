4b46451446aa9900d683228486f286fd8e6b513d
bayuapriansah <55168735+bayuapriansah@users.noreply.github.com>
2020-04-12 23:49:12 +0700

    First Code

---
+++ Check_Umur/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.CustomFormat = "MMMM yyyy"
+        Me.DateTimePicker1.DropDownAlign = System.Windows.Forms.LeftRightAlignment.Right
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker1.Location = New System.Drawing.Point(16, 31)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.ShowUpDown = True
+        Me.DateTimePicker1.Size = New System.Drawing.Size(214, 20)
+        Me.DateTimePicker1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 232)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(14, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "-"
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.CustomFormat = "MMMM yyyy"
+        Me.DateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker2.Location = New System.Drawing.Point(16, 84)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.ShowUpDown = True
+        Me.DateTimePicker2.Size = New System.Drawing.Size(214, 20)
+        Me.DateTimePicker2.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Tanggal Lahir"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 68)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(108, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Tanggal Pembanding"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 124)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(218, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Hitung"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(242, 261)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DateTimePicker2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Cek Umur"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DateTimePicker2 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Check_Umur/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    Public bulan, bulanHitung, DatPic1Bulan, DatPic2Bulan, tahun, DatPic1Tahun, DatPic2Tahun As Integer
+    Sub getNilaiSemua()
+        DatPic1Bulan = DateTimePicker1.Value.Month
+        DatPic1Tahun = DateTimePicker1.Value.Year
+        DatPic2Bulan = DateTimePicker2.Value.Month
+        DatPic2Tahun = DateTimePicker2.Value.Year
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
+        getNilaiSemua()
+        If DatPic2Tahun < DatPic1Tahun Then
+            MsgBox("Tahun Di Tanggal Pembanding Tidak Boleh Lebih Kecil Dari Tahun Lahir")
+            Label1.Text = "Ada Kesalahan Input"
+        Else
+            If DatPic1Bulan > DatPic2Bulan And DatPic1Tahun < DatPic2Tahun Then
+                tahun = (DatPic2Tahun - DatPic1Tahun) - 1
+                bulanHitung = DatPic2Bulan - DatPic1Bulan
+                bulan = 12 + bulanHitung
+            ElseIf DatPic1Bulan > DatPic2Bulan And DatPic1Tahun = DatPic2Tahun Then
+                MsgBox("Kamu Belum Lahir")
+            Else
+                tahun = DatPic2Tahun - DatPic1Tahun
+                bulan = DatPic2Bulan - DatPic1Bulan
+            End If
+            Label1.Text = "Umur Kamu " & tahun & " Tahun " & bulan & " Bulan"
+        End If
+    End Sub
+
+    Private Sub DateTimePicker_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged, DateTimePicker2.ValueChanged
+        getNilaiSemua()
+        If DatPic2Tahun < DatPic1Tahun Then
+            MsgBox("Tahun Di Tanggal Pembanding Tidak Boleh Lebih Kecil Dari Tahun Lahir")
+            Label1.Text = "Ada Kesalahan Input"
+        Else
+            If DatPic1Bulan > DatPic2Bulan And DatPic1Tahun < DatPic2Tahun Then
+                tahun = (DatPic2Tahun - DatPic1Tahun) - 1
+                bulanHitung = DatPic2Bulan - DatPic1Bulan
+                bulan = 12 + bulanHitung
+            ElseIf DatPic1Bulan > DatPic2Bulan And DatPic1Tahun = DatPic2Tahun Then
+                MsgBox("Kamu Belum Lahir")
+            Else
+                tahun = DatPic2Tahun - DatPic1Tahun
+                bulan = DatPic2Bulan - DatPic1Bulan
+            End If
+            Label1.Text = "Umur Kamu " & tahun & " Tahun " & bulan & " Bulan"
+        End If
+    End Sub
+End Class
